da56c333a58bcbd69371d4d69b19b3d84106b28e
Charles COQUE <coque.ext@secib.fr>
2021-02-22 14:20:20 +0100

    rename model temperatureC to temperatureCelsius

--- TestWebApiDocker/Controllers/WeatherForecastController.cs
+++ TestWebApiDocker/Controllers/WeatherForecastController.cs
@@ -32,7 +32,7 @@ namespace TestWebApiDocker.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
+                TemperatureCelsius = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
